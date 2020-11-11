41e0c5a6525096e7e9aface195d4f455db6147da
william ribeiro <william_jag_rs@hotmail.com>
2020-11-11 00:28:58 -0300

    finalizaçao part logica. Iniciando as correçoes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,83 +2,62 @@ const modelo = document.getElementById("inputModelo");
 const cor = document.getElementById("inputCor");
 const placa = document.getElementById("inputPlaca");
 const ano = document.getElementById("inputAno");
-const pesquisaPlacas = document.getElementById("inputPesquisaPlaca");
 
-//JSON.parse(localStorage.getItem("veiculosNoBox")) ||
-const veiculos = [];
-const vagas = [
-  { Vaga: "a1", Pos: 6 },
-  { Vaga: "a2", Pos: 5 },
-  { Vaga: "b1", Pos: 4 },
-  { Vaga: "b2", Pos: 3 },
-  { Vaga: "c1", Pos: 2 },
-  { Vaga: "c2", Pos: 1 },
+let busca = document.getElementById("busca");
+let resultBusca = document.getElementById("resulBusca");
+
+const relTop3 = document.getElementById("top3");
+const tempoBox = document.getElementById("inputTempoBox");
+
+const veiculos = JSON.parse(localStorage.getItem("veiculosNoBox")) || [];
+const vagas = JSON.parse(localStorage.getItem("vagas")) || [
+  { Vaga: "A1", Pos: 6 },
+  { Vaga: "A2", Pos: 5 },
+  { Vaga: "B1", Pos: 4 },
+  { Vaga: "B2", Pos: 3 },
+  { Vaga: "C1", Pos: 2 },
+  { Vaga: "C2", Pos: 1 },
 ];
 
-//const contabilVeiculos = JSON.parse(localStorage.getItem("contabil")) ||
-
-const contabilVeiculos = [
+const contabilVeiculos = JSON.parse(localStorage.getItem("contabil")) || [
   {
     Modelo: "",
     Cor: "",
     Placa: "",
     Ano: "",
-    Entrada: "",
     TotalPagamentos: "",
   },
 ];
 
 let box = [];
-let horaEntrada = [];
-let vPagar = "R$200,00";
-let tempoBox = "2 horas";
+let capturePlaca = [];
 let vaga = "";
-let totalPagamentos = 50;
+let totalPagamentos = 0;
 let vlPlaca = 0;
 let storageContabil = [];
-let indexVaga = 0;
-
-function relogio() {
-  var data = new Date();
-  var hor = data.getHours();
-  var min = data.getMinutes();
-  var seg = data.getSeconds();
-
-  if (hor < 10) {
-    hor = "0" + hor;
-  }
-  if (min < 10) {
-    min = "0" + min;
-  }
-  if (seg < 10) {
-    seg = "0" + seg;
-  }
-
-  var horas = hor + ":" + min + ":" + seg;
-  document.getElementById("horaAtual").innerHTML = horas;
-  horaEntrada = data;
-}
-var timer = setInterval(relogio, 1000);
+let res = 0;
+let msgBox = "";
+let fid = 0;
 
 function vagaLivre() {
   switch (vagas[0].Pos) {
     case 6:
-      vaga = "a1";
+      vaga = "A1";
       break;
     case 5:
-      vaga = "a2";
+      vaga = "A2";
       break;
     case 4:
-      vaga = "b1";
+      vaga = "B1";
       break;
     case 3:
-      vaga = "b2";
+      vaga = "B2";
       break;
     case 2:
-      vaga = "c1";
+      vaga = "C1";
       break;
     case 1:
-      vaga = "c2";
+      vaga = "C2";
       break;
 
     default:
@@ -90,52 +69,26 @@ function registrar() {
   if (vagas.length != 0) {
     vagaLivre();
     enterTheBox();
-    relogio();
 
     veiculos.push({
-      Modelo: modelo.value,
-      Cor: cor.value,
-      Placa: placa.value,
-      Ano: ano.value,
-      Vaga: vaga,
-      Entrada: horaEntrada,
+      Modelo: modelo.value.toUpperCase(),
+      Cor: cor.value.toUpperCase(),
+      Placa: placa.value.toUpperCase(),
+      Ano: ano.value.toUpperCase(),
+      Vaga: vaga.toUpperCase(),
     });
-
-    for (let index = 0; index < contabilVeiculos.length; index++) {
-      if (contabilVeiculos[index].Placa == placa.value) {
-        //FUNCAO NATIVA PARA EDITAR ITEM
-        contabilVeiculos.forEach((item) => {
-          item.TotalPagamentos = "100";
-        });
-        //
-        vlPlaca = 1;
-      }
-    }
-
-    if (vlPlaca == 0) {
-      contabilVeiculos.push({
-        Modelo: modelo.value,
-        Cor: cor.value,
-        Placa: placa.value,
-        Ano: ano.value,
-        Entrada: horaEntrada,
-        TotalPagamentos: totalPagamentos,
-      });
-    }
-
-    //localStorage.setItem("contabil", JSON.stringify(contabilVeiculos));
 
     vagas.splice(vagas.findIndex(find_vaga), 1);
 
-    vagas.sort(function (a, b) {
-      return a.Vaga > b.Vaga ? 1 : b.Vaga > a.Vaga ? -1 : 0;
-    });
+    ordenaVagas();
 
-    //localStorage.setItem("veiculosNoBox", JSON.stringify(veiculos));
+    localStorage.setItem("vagas", JSON.stringify(vagas));
+
+    localStorage.setItem("veiculosNoBox", JSON.stringify(veiculos));
   } else {
     alert("Todas as vagas estão ocupadas");
   }
-  vlPlaca = 0;
+
   clear();
 }
 
@@ -157,6 +110,7 @@ function enterTheBox() {
     name="customRadio"
     class="custom-control-input"
     data-box="${vaga}"
+    data-placa=${placa.value.toUpperCase()}
     onclick="captureId(event)"    
   />
   <label class="custom-control-label" for="customRadio${vaga}"
@@ -168,23 +122,17 @@ function enterTheBox() {
 }
 
 function start() {
-  count2 = 0;
   for (let index = 0; index < veiculos.length; index++) {
-    count2 = vagas[index].Pos;
     vaga = veiculos[index].Vaga;
     placa.value = veiculos[index].Placa;
     enterTheBox();
     placa.value = "";
   }
-  console.log(count2);
-  count2 = count2 - 1;
-  console.log(count2);
-  vaga = vagas[count2].Pos;
-  console.log(vaga);
 }
 
 function captureId(event) {
   box = event.target.dataset.box;
+  capturePlaca = event.target.dataset.placa;
 }
 
 function find_vaga(fVaga) {
@@ -196,35 +144,68 @@ function find_car(car) {
 }
 
 function find_placa(fPlaca) {
-  return fPlaca["Placa"] === buscaPlaca;
+  return fPlaca["Placa"] === capturePlaca;
+}
+
+function calcValor(tempo, horaBase, res) {
+  horaBase = 100.0;
+  tempo = tempoBox.value;
+  res = horaBase * tempo;
+  return res;
 }
 
 function exitBox() {
-  if (box != "") {
+  if (box != "" && tempoBox.value != "") {
     document.getElementById(box).style.display = "block";
     document.getElementById(`closed-${box}`).style.display = "none";
     document.getElementById(`removeRadio${box}`).remove();
+
+    res = calcValor(parseInt(tempoBox.value));
+    descontoAno();
+
+    for (let index = 0; index < contabilVeiculos.length; index++) {
+      if (contabilVeiculos[index].Placa == capturePlaca) {
+        top3();
+
+        vlPlaca = 1;
+      }
+    }
+
+    if (vlPlaca == 0) {
+      pio = veiculos.findIndex(find_placa);
+
+      contabilVeiculos.push({
+        Modelo: veiculos[pio].Modelo,
+        Cor: veiculos[pio].Cor,
+        Placa: veiculos[pio].Placa,
+        Ano: veiculos[pio].Ano,
+        TotalPagamentos: res,
+      });
+    }
+    ordenaContabil();
+
+    localStorage.setItem("contabil", JSON.stringify(contabilVeiculos));
 
     //Procura carro com a garagem e remove
     veiculos.splice(veiculos.findIndex(find_car), 1);
     //
     switch (box) {
-      case "a1":
+      case "A1":
         position = 6;
         break;
-      case "a2":
+      case "A2":
         position = 5;
         break;
-      case "b1":
+      case "B1":
         position = 4;
         break;
-      case "b2":
+      case "B2":
         position = 3;
         break;
-      case "c1":
+      case "C1":
         position = 2;
         break;
-      case "c2":
+      case "C2":
         position = 1;
         break;
 
@@ -237,31 +218,30 @@ function exitBox() {
       Pos: position,
     });
 
-    vagas.sort(function (a, b) {
-      return a.Vaga > b.Vaga ? 1 : b.Vaga > a.Vaga ? -1 : 0;
-    });
+    ordenaVagas();
+    localStorage.setItem("vagas", JSON.stringify(vagas));
+    localStorage.setItem("veiculosNoBox", JSON.stringify(veiculos));
 
-    alert(`Atenção você ficou ${tempoBox} e deverá pagar ${vPagar}`);
+    vlPlaca = 0;
   } else if (vagas.length == 6) {
     alert("Atenção todas as vagas estão livres");
   } else {
-    alert("Você não selecionou nenhum veículo");
+    alert("Selecione o veículo e preencha o tempo de permanência");
   }
   box = "";
+  tempoBox.value = "";
+  mostraTop3();
 }
 
 function pesquisaModelo() {
-  let busca = document.getElementById("busca");
-  let resultBusca = document.getElementById("resulBusca");
-  let pesquisaModelo = document.getElementById("inputPesquisaModelo");
-
+  const pesquisaModelo = document.getElementById("inputPesquisaModelo");
   if (veiculos.length != 0) {
-    if (pesquisaModelo.value != "") {
+    if (pesquisaModelo.value.toUpperCase() != "") {
       resultBusca.innerHTML = "";
       busca.innerHTML = "";
       for (let index = 0; index < veiculos.length; index++) {
-        if (veiculos[index].Modelo == pesquisaModelo.value) {
-          busca.innerHTML = `<h5 id="busca" class="text-center">Os modelos ${pesquisaModelo.value} estão localizados em:</h5>`;
+        if (veiculos[index].Modelo == pesquisaModelo.value.toUpperCase()) {
+          busca.innerHTML = `<h5 id="busca" class="text-center">O modelo ${pesquisaModelo.value.toUpperCase()} está localizado em:</h5>`;
 
           resultBusca.innerHTML += `<h4 class="text-center">${veiculos[index].Vaga}</h4>`;
         }
@@ -276,22 +256,20 @@ function pesquisaModelo() {
 }
 
 function pesquisaCor() {
-  let busca = document.getElementById("busca");
-  let resultBusca = document.getElementById("resulBusca");
   let pesquisaCor = document.getElementById("inputPesquisaCor");
   if (veiculos.length != 0) {
-    if (pesquisaCor.value != "") {
+    if (pesquisaCor.value.toUpperCase() != "") {
       resultBusca.innerHTML = "";
       busca.innerHTML = "";
       for (let index = 0; index < veiculos.length; index++) {
-        if (veiculos[index].Cor == pesquisaCor.value) {
-          busca.innerHTML = `<h5 id="busca" class="text-center">Os veículos de cor ${pesquisaCor.value} estão localizados em:</h5>`;
+        if (veiculos[index].Cor == pesquisaCor.value.toUpperCase()) {
+          busca.innerHTML = `<h5 id="busca" class="text-center">Os veículos de cor ${pesquisaCor.value.toUpperCase()} estão localizados em:</h5>`;
 
           resultBusca.innerHTML += `<h4 class="text-center">${veiculos[index].Vaga}</h4>`;
         }
       }
     } else {
-      alert("Atenção campo Modelo não pode ser em branco");
+      alert("Atenção campo Cor não pode ser em branco");
     }
   } else {
     alert("Atenção todas as vagas estão livres");
@@ -300,22 +278,21 @@ function pesquisaCor() {
 }
 
 function pesquisaPlaca() {
-  let busca = document.getElementById("busca");
-  let resultBusca = document.getElementById("resulBusca");
+  const pesquisaPlacas = document.getElementById("inputPesquisaPlaca");
 
   if (veiculos.length != 0) {
-    if (pesquisaPlacas.value != "") {
+    if (pesquisaPlacas.value.toUpperCase() != "") {
       resultBusca.innerHTML = "";
       busca.innerHTML = "";
       for (let index = 0; index < veiculos.length; index++) {
-        if (veiculos[index].Placa == pesquisaPlacas.value) {
-          busca.innerHTML = `<h5 id="busca" class="text-center">Carro de Placa ${pesquisaPlacas.value} está localizado o vaga:</h5>`;
+        if (veiculos[index].Placa == pesquisaPlacas.value.toUpperCase()) {
+          busca.innerHTML = `<h5 id="busca" class="text-center">Carro de Placa ${pesquisaPlacas.value.toUpperCase()} está localizado o vaga:</h5>`;
 
           resultBusca.innerHTML = `<h4 class="text-center">${veiculos[index].Vaga} </h4>`;
         }
       }
     } else {
-      alert("Atenção campo Modelo não pode ser em branco");
+      alert("Atenção campo Placa não pode ser em branco");
     }
   } else {
     alert("Atenção todas as vagas estão livres");
@@ -324,23 +301,21 @@ function pesquisaPlaca() {
 }
 
 function pesquisaVaga() {
-  let busca = document.getElementById("busca");
-  let resultBusca = document.getElementById("resulBusca");
-  let pesquisaVaga = document.getElementById("inputPesquisaVaga");
+  const pesquisaVaga = document.getElementById("inputPesquisaVaga");
 
   if (veiculos.length != 0) {
-    if (pesquisaVaga.value != "") {
+    if (pesquisaVaga.value.toUpperCase() != "") {
       resultBusca.innerHTML = "";
       busca.innerHTML = "";
       for (let index = 0; index < veiculos.length; index++) {
-        if (veiculos[index].Vaga == pesquisaVaga.value) {
-          busca.innerHTML = `<h5 id="busca" class="text-center">Na Vaga ${pesquisaVaga.value} está localizado o Veículo:</h5>`;
+        if (veiculos[index].Vaga == pesquisaVaga.value.toUpperCase()) {
+          busca.innerHTML = `<h5 id="busca" class="text-center">Na Vaga ${pesquisaVaga.value.toUpperCase()} está localizado o Veículo:</h5>`;
 
           resultBusca.innerHTML = `<h4 class="text-center">Modelo ${veiculos[index].Modelo} de Placa: ${veiculos[index].Placa}</h4>`;
         }
       }
     } else {
-      alert("Atenção campo Modelo não pode ser em branco");
+      alert("Atenção campo Vaga não pode ser em branco");
     }
   } else {
     alert("Atenção todas as vagas estão livres");
@@ -348,37 +323,146 @@ function pesquisaVaga() {
   pesquisaVaga.value = "";
 }
 
-function desconto() {
-  let pesquisaVaga = document.getElementById("inputPesquisaVaga");
-
-  if (pesquisaVaga.value > 2015) {
-    alert(`Ano maior que 2015 não tem desconto. Valor à Pagar: ${vPagar}`);
-  } else if (pesquisaVaga.value <= 2015 && pesquisaVaga.value >= 2010) {
-    alert("Você tem desconto de 15%");
-  } else if (pesquisaVaga.value <= 2009 && pesquisaVaga.value >= 2000) {
-    alert("Você tem desconto de 20%");
-  } else if (pesquisaVaga.value < 2000) {
-    alert("Você tem desconto de 30%");
+function descontoAno() {
+  for (index in veiculos) {
+    if (veiculos[index].Placa == capturePlaca) {
+      if (parseInt(veiculos[index].Ano) > 2015) {
+        msgBox = `Você ficou ${tempoBox.value}h. \n\ `;
+      } else if (
+        parseInt(veiculos[index].Ano) <= 2015 &&
+        parseInt(veiculos[index].Ano) >= 2010
+      ) {
+        res -= 0.15 * res;
+        msgBox = `Você ficou ${tempoBox.value}h. \n\nValor à pagar com desconto de 15% pelo ano.\n`;
+      } else if (
+        parseInt(veiculos[index].Ano) <= 2009 &&
+        parseInt(veiculos[index].Ano) >= 2000
+      ) {
+        res -= 0.2 * res;
+        msgBox = `Você ficou ${tempoBox.value}h. \n\nValor à pagar com desconto de 20% pelo ano.\n`;
+      } else if (parseInt(veiculos[index].Ano) < 2000) {
+        res -= 0.3 * res;
+        msgBox = `Você ficou ${tempoBox.value}h. \n\nValor à pagar com desconto de 30% pelo ano.\n`;
+      }
+    }
   }
+  descontoCor();
 }
 
 function descontoCor() {
-  let pesquisaVaga = document.getElementById("inputPesquisaVaga");
+  for (index in veiculos) {
+    if (veiculos[index].Placa == capturePlaca) {
+      if (veiculos[index].Cor == "AZUL") {
+        res -= 10;
+        msgBox += `\n + R$ 10,00 de desconto por cor. \nValor total ${res.toLocaleString(
+          "pt-br",
+          { style: "currency", currency: "BRL" }
+        )}`;
+      } else if (veiculos[index].Cor == "BRANCO") {
+        res -= 15;
+        msgBox += `\n + R$ 15,00 de desconto por cor. \nValor total ${res.toLocaleString(
+          "pt-br",
+          { style: "currency", currency: "BRL" }
+        )}`;
+      } else if (veiculos[index].Cor == "PRETO") {
+        res -= 20;
+        msgBox += `\n + R$ 20,00 de desconto por cor. \nValor total ${res.toLocaleString(
+          "pt-br",
+          { style: "currency", currency: "BRL" }
+        )}`;
+      } else {
+        msgBox += `\n Valor total ${res.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}`;
+      }
+    }
+  }
+  fidelidade();
+}
 
-  if (pesquisaVaga.value == "azul") {
-    alert(`Você tem 10 reais de desconto`);
-  } else if (pesquisaVaga.value == "branco") {
-    alert("Você tem 15 reais de desconto");
-  } else if (pesquisaVaga.value == "preto") {
-    alert("Você tem 20 reais de desconto");
-  } else if (
-    pesquisaVaga.value != "azul" ||
-    pesquisaVaga.value != "branco" ||
-    pesquisaVaga.value != preto
-  ) {
-    alert("Você não tem Desconto");
+function fidelidade() {
+  for (index in contabilVeiculos) {
+    if (contabilVeiculos[index].Placa == capturePlaca) {
+      res -= 0.5 * res;
+      alert(
+        `${msgBox}\n\n + 50% desconto fidelidade. Valor à pagar: ${res.toLocaleString(
+          "pt-br",
+          { style: "currency", currency: "BRL" }
+        )}`
+      );
+      fid = 1;
+    }
+  }
+  if (fid == 0) {
+    alert(msgBox);
   }
 }
 
-relogio();
-//start();
+function ordenaContabil() {
+  contabilVeiculos.sort(function (a, b) {
+    return a.TotalPagamentos > b.TotalPagamentos
+      ? -1
+      : b.TotalPagamentos > a.TotalPagamentos
+      ? 1
+      : 0;
+  });
+}
+
+function ordenaVagas() {
+  vagas.sort(function (a, b) {
+    return a.Vaga > b.Vaga ? 1 : b.Vaga > a.Vaga ? -1 : 0;
+  });
+}
+function top3() {
+  let newCar = [];
+
+  contabilVeiculos.forEach(myFunction);
+
+  function myFunction(item, index, arr) {
+    arr[index] = item;
+    if (item.Placa == capturePlaca) {
+      cx = item.TotalPagamentos;
+
+      newCar.push(arr[index]);
+
+      arr.splice(index, 1);
+      cx += res;
+
+      contabilVeiculos.push({
+        Modelo: newCar[0].Modelo,
+        Cor: newCar[0].Cor,
+        Placa: newCar[0].Placa,
+        Ano: newCar[0].Ano,
+        TotalPagamentos: cx,
+      });
+
+      newCar.slice(0, 1);
+      ordenaContabil();
+      localStorage.setItem("contabil", JSON.stringify(contabilVeiculos));
+    }
+  }
+
+  console.log(contabilVeiculos);
+}
+
+function mostraTop3() {
+  cc = 1;
+  relTop3.innerHTML = "";
+  for (let index = 0; index < 3; index++) {
+    relTop3.innerHTML += `<h5 class="text-center">${cc}º -${
+      contabilVeiculos[index].Modelo
+    } - Ano: ${contabilVeiculos[index].Ano} - Placa: ${
+      contabilVeiculos[index].Placa
+    } | Total Gasto R$ ${contabilVeiculos[index].TotalPagamentos.toLocaleString(
+      "pt-br",
+      {
+        style: "currency",
+        currency: "BRL",
+      }
+    )}</h5>`;
+    cc++;
+  }
+}
+start();
+mostraTop3();
